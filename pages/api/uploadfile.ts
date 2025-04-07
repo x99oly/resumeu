97b7544af2524716a5c8c59957baa5d2e44132cd
Sameu Araujo <oliveira.samuel.edu@gmail.com>
2025-04-07 00:16:50 -0300

    texto sendo recuperado do pdf com sucesso

--- pages/api/uploadfile.ts
+++ pages/api/uploadfile.ts
@@ -1,11 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import pdfParse from "pdf-parse"
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
 
 export default async function handler(
-    req:NextApiRequest,
-    res:NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    if (req.method !== "POST") return res.status(400).end()
-    const data = req.body
-    const id = 2
-    res.status(200).json(data)
+  if (req.method !== "POST") return res.status(400).end()
+
+  const chunks: Uint8Array[] = []
+
+  req.on("data", (chunk) => chunks.push(chunk))
+  req.on("end", async () => {
+    const buffer = Buffer.concat(chunks)
+    const data = await pdfParse(buffer)
+    res.status(200).json({ text: data.text })
+  })
 }
